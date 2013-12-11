f5e9e76ea4220fa1aef8be2f23dad7e78989c427
siepet <siepet7@gmail.com>
2013-12-11 13:37:43 +0100

    omfg

--- atoi.asm
+++ atoi.asm
@@ -10,19 +10,21 @@ putchar	MACRO	k
 	int 10h
 	ENDM
 
-Atoi	SEGMENT
-	ASSUME cs: Atoi, ds: Atoi, es: Atoi, ss: Atoi
-	ORG 100h
+AATOI	SEGMENT
+		ASSUME cs: AATOI, ds: AATOI, es: AATOI, ss: AATOI
+		ORG 100h
 
 start:
-	mov bx, offset string
-	call atoi
+		;mov bx, offset string
+		lea bx, string
+		call atoi
+		call hex
+		putchar 10	; enter
+		putchar 13	; enter
+		exit 0
 
-	putchar 65	; prints capital letter 'a'
-	exit 0
-
-string:	DB	"12345",0
-
+string:	DB	"65535",0				; 3039h
+			
 ; *****************************************************************
 ;	ASCII TO INTEGER PRODECURE
 ;
@@ -33,26 +35,171 @@ string:	DB	"12345",0
 
 
 atoi	PROC
-	mov si, 0
-	mov ax, 0
-	mov cx, 10
+		mov si, 0
+		xor ax, ax
+		mov cx, 0Ah
+		xor dx, dx
 
 s:
-	mov dl, [bx+si]
-	cmp dl, 0
-	je k
+		mov dl, [bx+si]			; al == 31
+		cmp dl, 0
+		je k
 
-	sub dl, 48
-	mul cx
-	add ax, dl
-	;lea ax, [ax*10+dl] ; (??) it works?
-	inc si
-	jmp s
+		sub dl, 30h				; al == 1			
+		push dx
+		mul cx					; al * 10 == 
+		pop dx
+		
+		add ax, dx
+		inc si		
+		jmp s
 
 k:
-	ret
+		
+		ret
 atoi	ENDP
+deci2	PROC
+		push ax
+		; ax = 65535
+		xor dx, dx
+		mov cx, 10000
+		div cx
+		; DX = DX:AX % cx ; this case: 5535
+		; AX = DX:AX / cx ; this case: 6
+		; mov ax, 5
+		; 0000 0000 0000 0110
+		; 0000 0000 AH 
+		; 	
+		add al, 48
+		putchar al
 
+				
+		mov ax, dx
+		xor dx, dx
+		mov cx, 1000
+		div cx
+		add al, 48
+		putchar al
+		
+		mov ax, dx
+		xor dx, dx
+		mov cx, 100
+		div cx
+		add al, 48
+		putchar al
+		
+		mov ax, dx
+		xor dx, dx
+		mov cx, 10
+		div cx
+		add al, 48
+		putchar al
+		
+		mov ax, dx
+		xor dx, dx
+		mov cx, 1
+		div cx
+		add al, 48
+		putchar al
+		
+		
+		pop ax
+		ret
+deci2	ENDP
 
-Atoi	ENDS
-	END start
+; *********************************************************************
+	; Opis:
+	; Prodecura wypisuje liczbę z rejestru ax w systemie heksadecymalnym.
+	; PUSH-ując rejestry modyfikowane wewnątrz i POP-ując je przed zakończeniem.
+
+	; Wejście:
+	; Przyjmowany na wejsciu jest 16-bitowy rejestr ax, w którym znajduje się liczba
+
+	; Wyjście:
+	; Wypisana liczba heksadecymalna na stdout.
+
+; *********************************************************************
+
+hex 	PROC 	
+
+		push ax
+		push bx
+		push cx
+		push dx
+		
+		mov dx,ax
+		mov dx,ax
+		
+		mov bx,dx
+		shr bx,12
+		and bx,15
+		mov ax,bx
+		
+		cmp ax,10
+		jge yyy
+		add al,48
+		sub al, 55
+yyy:
+		add al, 55
+		
+		mov ah, 0Eh
+		int 10h
+		
+		mov bx,dx
+		shr bx,8
+		and bx,15
+		mov ax,bx
+		
+		cmp ax,10
+		jge yyz
+		add al,48
+		sub al, 55
+yyz:
+		add al, 55
+		
+		mov ah, 0Eh
+		int 10h
+		
+		mov bx,dx
+		shr bx,4
+		and bx,15
+		mov ax,bx
+		
+		cmp ax,10
+		jge yya
+		add al,48
+		sub al, 55
+yya:
+		add al, 55
+		
+		mov ah, 0Eh
+		int 10h
+		
+
+		
+		mov cl, 0
+		mov bx,dx
+		shr bx,cl
+		and bx,15
+		mov ax,bx
+		
+		cmp ax,10
+		jge yyb
+		add al,48
+		sub al, 55
+yyb:
+		add al, 55
+		
+		mov ah, 0Eh
+		int 10h
+		
+		pop dx
+		pop bx
+		pop cx
+		pop ax
+		
+		ret
+hex		ENDP
+
+AATOI	ENDS
+		END start
--- batch.bat
+++ batch.bat
@@ -1,1 +1,2 @@
-start batch.bat
+X:\COM\BC31\BIN\tasm H:\asm\%1.asm H:\asm
+X:\COM\BC31\BIN\tlink H:\asm\%1 -t
